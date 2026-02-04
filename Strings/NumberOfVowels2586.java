42cf5578d3ae27f7505cb6f277a91d11a5438f84
Ayush <151222788+AyushC2@users.noreply.github.com>
2026-02-04 10:46:04 +0530

    NumberOfVowels2586.java

---
+++ Strings/NumberOfVowels2586.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isVowel(char c) {
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        };
+        return false;
+    }
+    public int vowelStrings(String[] words, int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            String w = words[i];
+            char first = w.charAt(0);
+            char last = w.charAt(w.length() - 1);
+            if (isVowel(first) && isVowel(last)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
